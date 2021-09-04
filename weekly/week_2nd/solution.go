192f62932867690c790a1d91cc11b46dc1261a00
gurumee <gurumee92@gmail.com>
2021-09-04 15:34:29 +0900

    Add week_2nd

---
+++ weekly/week_2nd/solution.go
@@ -0,0 +1,62 @@
+package week_2nd
+
+func scorePointToGrade(score int) string {
+	if score >= 90 {
+		return "A"
+	} else if score >= 80 {
+		return "B"
+	} else if score >= 70 {
+		return "C"
+	} else if score >= 50 {
+		return "D"
+	} else {
+		return "F"
+	}
+}
+
+func solution(scores [][]int) string {
+	n := len(scores)
+	result := ""
+
+	for i:=0; i<n; i++ {
+		myScore := scores[i][i]
+		sum := myScore
+		max := myScore
+		min := myScore
+		sameCnt := 0
+
+		for j:=0; j<n; j++ {
+			if i == j {
+				continue
+			}
+
+			score := scores[j][i]
+			sum += score
+
+			if max < score {
+				max = score
+			}
+
+			if min > score {
+				min = score
+			}
+
+			if myScore == score {
+				sameCnt += 1
+			}
+		}
+
+		average := 0
+
+		if ( max == myScore || min == myScore ) && sameCnt == 0  {
+			sum -= myScore
+			average = sum / (n - 1)
+		} else {
+			average = sum / n
+		}
+
+		result += scorePointToGrade(average)
+	}
+
+	return result
+}
---
+++ weekly/week_2nd/solution_test.go
@@ -0,0 +1,88 @@
+package week_2nd
+
+import "testing"
+
+func TestScorePointToGrade(t *testing.T) {
+	var tests = []struct {
+		score   int
+		expected string
+	}{
+		{
+			90,
+			"A",
+		},
+		{
+			95,
+			"A",
+		},
+		{
+			80,
+			"B",
+		},
+		{
+			70,
+			"C",
+		},
+		{
+			60,
+			"D",
+		},
+		{
+			50,
+			"D",
+		},
+		{
+			49,
+			"F",
+		},
+	}
+
+	for _, test := range tests {
+		result := scorePointToGrade(test.score)
+
+		if test.expected != result {
+			t.Errorf("Wrong result expected: %v result: %v\n", test.expected, result)
+		}
+	}
+}
+
+func TestSolution(t *testing.T) {
+	var tests = []struct {
+		scores   [][]int
+		expected string
+	}{
+		{
+			[][]int{
+				{100, 90, 98, 88, 65},
+				{50, 45, 99, 85, 77},
+				{47, 88, 95, 80, 67},
+				{61, 57, 100, 80, 65},
+				{24, 90, 94, 75, 65},
+			},
+			"FBABD",
+		},
+		{
+			[][]int{
+				{50, 90},
+				{50, 87},
+			},
+			"DA",
+		},
+		{
+			[][]int{
+				{70, 49, 90},
+				{68, 50, 38},
+				{73, 31, 100},
+			},
+			"CFD",
+		},
+	}
+
+	for _, test := range tests {
+		result := solution(test.scores)
+
+		if test.expected != result {
+			t.Errorf("Wrong result expected: %v result: %v\n", test.expected, result)
+		}
+	}
+}
